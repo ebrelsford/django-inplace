c3f255f8d3c6303909307b538c7627fe54e34ffa
Eric Brelsford <ebrelsford@gmail.com>
2014-03-22 20:52:40 -0400

    BaseGetBoundaryTag

--- inplace/boundaries/templatetags/boundaries_tags.py
+++ inplace/boundaries/templatetags/boundaries_tags.py
@@ -1,6 +1,34 @@
+from django.contrib.gis.geos import Point
+
 from classytags.arguments import Argument
 from classytags.core import Options
 from classytags.helpers import AsTag
+
+
+class BaseGetBoundaryTag(AsTag):
+    """
+    A base tag for finding the boundary that a point is in.
+    """
+    options = Options(
+        Argument('lat'),
+        Argument('lon'),
+        'as',
+        Argument('varname', resolve=False, required=False),
+    )
+
+    def get_boundary_model(self):
+        raise NotImplementedError('Implement get_boundary_model()')
+
+    def get_value(self, context, lat, lon):
+        boundary_model = self.get_boundary_model()
+        try:
+            return boundary_model.objects.get(
+                geometry__contains=Point(lon, lat, srid=4326)
+            )
+        except boundary_model.DoesNotExist:
+            return None
+        except boundary_model.MultipleObjectsReturned:
+            return None
 
 
 class BaseAllBoundariesTag(AsTag):
